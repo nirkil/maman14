3892d575c3fff35435cc1ed24a4240c7f506c8a5
nirkil <101107615+nirkil@users.noreply.github.com>
2022-03-17 17:53:12 +0200

    Update main.c

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 static bool process_file(char *filename) {
 	
 	int temp_c;
-	long ic = IC_INIT_VALUE, dc = 0, icf, dcf; /* Memory address counters */
+	long ic = IC_VALUE, dc = 0, icf, dcf; /* Memory address counters */
 
 	bool checking_success = TRUE; /* for chechking later if the pass are correct*/
     
@@ -15,11 +15,13 @@ static bool process_file(char *filename) {
 	FILE *fd; /* Current assembly file descriptor to process after the first pass of macros*/
 
 	long data_img[8191]; /* Contains an image of the machine code ???*/
-	machine_בםגק *code_img[8191];
+	machine_code *code_img[8191];
 	
 	table symbol_table = NULL; /* The symbol table */
 	line_info curr_line_info;
- 
+    
+	input_filename = addext(filename, ".as");
+
 	fd = fopen(input_filename, "r"); /*  Open file, after macros */
 
 	if (fd == NULL) { /* if file couldn't be opened, write to stderr */
@@ -27,9 +29,8 @@ static bool process_file(char *filename) {
 		free(input_filename); 
 		return FALSE;
     }
-}
- 
-/* start second pass*/
+    
+     /* start first pass*/
 
 	curr_line_info.file_name = input_filename;
 	curr_line_info.content = temp_line; /* We use temp_line to read from the file, but it stays at same location. */
@@ -47,11 +48,40 @@ static bool process_file(char *filename) {
 			} while (temp_c != '\n' && temp_c != EOF);
 
 		} else {
-			if (!process_line_fpass(curr_line_info, &ic, &dc, code_img, data_img, &symbol_table)) {
-				if (is_success) {
+			if (!process_line_fpass(curr_line_info, &ic, &dc, code_img, data_img, &symbol_table)) 
+			{ /* steps 1.01 -1.17 in algo */
+				if (is_success) 
+				{
 					/*free_code_image(code_img, ic_before);*/
 					icf = -1;
 					is_success = FALSE;
 				}
 			}
 		}
+    /* Save ICF & DCF step 1.18 in the algo */
+	icf = ic;
+	dcf = dc;
+
+	/* if first pass didn't fail, start the second pass */
+	if (is_success) {
+
+	ic = IC_INIT_VALUE;
+
+	/* step 1.19 in the algo */
+	add_value_to_type(symbol_table, icf, DATA_SYMBOL);
+	  
+   /*second pass */
+
+	/* Write files if second pass succeeded */
+	if (is_success) 
+	{
+		/* Everything was done. Write to *filename.ob/.ext/.ent */
+		is_success = write_output_files(code_img, data_img, icf, dcf, filename, symbol_table);
+	}
+	
+	free(input_filename);
+	free_table(symbol_table);
+	free_code_image(code_img, icf);
+
+	return is_success;
+}
