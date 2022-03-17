0554262d58761929d654766ecd40f17b214db91e
Noga-R <72741533+Noga-R@users.noreply.github.com>
2022-03-17 17:37:39 +0200

    Add files via upload

--- second_pass.c
+++ second_pass.c
@@ -28,13 +28,15 @@ void second_pass_handle(FILE *input_file) {
 					break;
 				}
 
-				else if (is_entry(word)) {
-					add_entry_to_symbol_table(word);
+				else if (strcmp(word, ".entry")) {
+					char *symbol = (char*) malloc(sizeof(char));
+					symbol = get_entry_symbol(line_copy);
+					add_entry_to_symbol_table(symbol_table, symbol); /*TODO: <-- */
 					break;
 				}
 
 				else {
-					complete_assembler_table();
+					complete_assembler_table(assembler_table, symbol_table); /*TODO: <-- */
 				}
 
 				word = strtok(NULL, " \t");
@@ -43,24 +45,98 @@ void second_pass_handle(FILE *input_file) {
 
 	}
 
-	create_output_files();
+	if (is_entey(input_file)) {
+		FILE *entries_output_file;
+		entries_output_file = fopen("entries.ent", "w");
+		fill_entries_output_file(symbol_table, entries_output_file);
+		fclose(entries_output_file);
+	}
+
+	if (is_external(input_file)) {
+		FILE *extern_output_file;
+		extern_output_file = fopen("externals.ext", "w");
+		fill_externals_output_file(assembler_table, entries_output_file);
+		fclose(extern_output_file);
+	}
+
+	FILE *object_output_file;
+	object_output_file = fopen("object.ob", "w");
+	fill_object_output_file(assembler_table, object_output_file);
+	fclose(object_output_file);
+}
+
+void add_entry_to_symbol_table(char *word) {
+	/*add_table_item(symbol,*DC,base ,offset, attrinutes,symbol_table);
+	 * add_table_item(symbol_table, symbol, 0, EXTERNAL_SYMBOL)
+	 *  TODO: delete*/
+	add_table_item();
+}
+
+void complete_assembler_table( assembler_table, symbol_table) {
 
 }
 
-void add_entry_to_symbol_table(word){
+char* get_entry_symbol(char *line){ /*TODO: check function */
+	char *word = (char*) malloc(sizeof(char));
+	word = strtok(line, " \t");
+	word = strtok(NULL, " \t");
+	word = strtok(line, " \t");
+	return word;
+}
+
+int is_entey(FILE input_file) {
+	char *word;
+	char line[MAX_LINE_LENGTH];
+
+	while (feof(input_file) == 0) {
+		if (fgets(line, MAX_LINE_LENGTH, input_file) != NULL) {
+			if (line[0] != ';' && line[0] != '\n') {
+				word = strtok(line, " \t");
+				while (word != NULL) {
+					if (strcmp(word, ".extrn") == 0) {
+						return 1;
+					}
+				}
+			}
+		}
+	}
+	return 0;
+}
+
+int is_external(FILE input_file) {
+	char *word;
+	char line[MAX_LINE_LENGTH];
+
+	while (feof(input_file) == 0) {
+		if (fgets(line, MAX_LINE_LENGTH, input_file) != NULL) {
+			if (line[0] != ';' && line[0] != '\n') {
+				word = strtok(line, " \t");
+				while (word != NULL) {
+					if (strcmp(word, ".entry") == 0) {
+						return 1;
+					}
+				}
+			}
+		}
+	}
+	return 0;
+}
+
+void fill_entries_output_file(symbol_table, FILE *entries_output_file) {/*TODO: <-- */
 
 }
 
-void complete_assembler_table(){
+void fill_externals_output_file(assembler_table, FILE *externals_output_file) {/*TODO: <-- */
 
 }
 
-FILE* create_output_files(){
-
+void fill_object_output_file(assembler_table, FILE *object_output_file) {/*TODO: <-- */
 }
 
+
+/* TODO: delete
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	puts("!!!Hello World!!!");
 	return EXIT_SUCCESS;
-}
+}*/
 
---
+++ second_pass.h
@@ -0,0 +1,24 @@
+
+
+#ifndef SEC_PASS
+#define SEC_PASS
+
+/*operates the second pass: gets input files and creates necessary output files*/
+void second_pass_handle(FILE *input_file);
+
+/*adds the word entry in given symbol line of symbol table*/
+void add_entry_to_symbol_table(char *symbol);
+
+/*fills in the empty assembler table line after the first pass*/
+void complete_assembler_table(assembler_table, symbol_table);
+
+/*fills entry output file*/
+void fill_entries_output_file(symbol_table, FILE *entries_output_file);
+
+/*fills externals output file*/
+void fill_externals_output_file(assembler_table, FILE *externals_output_file);
+
+/*fills object output file*/
+void fill_object_output_file(assembler_table, FILE *object_output_file);
+
+#endif
