b9243c817d7520f361d7a19a440b9a090bc303cc
Noga-R <72741533+Noga-R@users.noreply.github.com>
2022-03-18 15:35:53 +0200

    Add files via upload

--- second_pass.c
+++ second_pass.c
@@ -4,9 +4,9 @@
 #include "globals.h"
 #include "utils.h"
 #include "SymbolTable.h"
-#include "second_pass.h"
 
-void second_pass_handle(FILE *input_file) {
+void second_pass_handle(FILE *input_file, machine_code assembler_table,
+		table symbol_table, char *input_file_name) {
 
 	char line[MAX_LINE_LENGTH];
 	char line_copy[MAX_LINE_LENGTH];
@@ -29,15 +29,15 @@ void second_pass_handle(FILE *input_file) {
 					break;
 				}
 
-				else if (strcmp(word, ".entry")) {
+				else if (strcmp(word, ".entry")) { /*checks to see if it's entry*/
 					char *symbol = (char*) malloc(sizeof(char));
 					symbol = get_entry_symbol(line_copy);
-					add_entry_to_symbol_table(symbol_table, symbol); /*TODO: <-- */
+					add_entry_to_symbol_table(symbol_table, symbol);
 					break;
 				}
 
 				else {
-					complete_assembler_table(assembler_table, symbol_table); /*TODO: <-- */
+					complete_assembler_table(assembler_table, symbol_table, word); /*TODO: <-- */
 				}
 
 				word = strtok(NULL, " \t");
@@ -45,39 +45,68 @@ void second_pass_handle(FILE *input_file) {
 		}
 
 	}
+	char *input_name = (char*) malloc(sizeof(char));
+	input_name = cut_input_file_name_ending(input_file_name); /*clear '.as' ending from input file name*/
 
-	if (is_entey(input_file)) {
+	if (is_entey(input_file)) { /*create and fill entry output file*/
 		FILE *entries_output_file;
-		entries_output_file = fopen("entries.ent", "w");
+		char *input_name_ent = (char*) malloc(sizeof(char));
+		strcpy(input_name_ent, input_name);
+		strcat(input_name_ent, ".ent"); /*add '.ent' ending to file name*/
+		entries_output_file = fopen(input_name_ent, "w");
 		fill_entries_output_file(symbol_table, entries_output_file);
 		fclose(entries_output_file);
 	}
 
-	if (is_external(input_file)) {
+	if (is_external(input_file)) { /*create and fill ext output file*/
 		FILE *extern_output_file;
-		extern_output_file = fopen("externals.ext", "w");
-		fill_externals_output_file(assembler_table, entries_output_file);
+		char *input_name_ext = (char*) malloc(sizeof(char));
+		strcpy(input_name_ext, input_name);
+		strcat(input_name_ext, ".ext"); /*add '.ext' ending to file name*/
+		extern_output_file = fopen(input_name_ext, "w");
+		fill_externals_output_file(assembler_table, extern_output_file);
 		fclose(extern_output_file);
 	}
 
+	/*create and fill object output file*/
 	FILE *object_output_file;
-	object_output_file = fopen("object.ob", "w");
+	char *input_name_ob = (char*) malloc(sizeof(char));
+	strcpy(input_name_ob, input_name);
+	strcat(input_name_ob, ".ob"); /*add '.ob' ending to file name*/
+	object_output_file = fopen(input_name_ob, "w");
 	fill_object_output_file(assembler_table, object_output_file);
 	fclose(object_output_file);
 }
 
-void add_entry_to_symbol_table(char *word) {
-	/*add_table_item(symbol,*DC,base ,offset, attrinutes,symbol_table);
-	 * add_table_item(symbol_table, symbol, 0, EXTERNAL_SYMBOL)
-	 *  TODO: delete*/
-	add_table_item();
+void add_entry_to_symbol_table(table symbol_table, char *symbol) {
+	table *current = symbol_table;
+	while (strcmp(current != NULL && current->key, symbol) != 0) {
+		current = current->next;
+	}
+	if (current == NULL) {
+		printf("ERROR! symbol: %s not found in symbol table", symbol);
+		break;
+	} else {
+		/*TODO: enter entry to attribute*/
+	}
 }
 
-void complete_assembler_table( assembler_table, symbol_table) {
+int is_symbol(table *symbol_table, char *word){
+	table *current = symbol_table;
+	while(current != NULL){
+		if(strcmp(current->key, word) == 0){
+			return 1;
+		}
+		current = current->next;
+	}
+	return 0;
+}
+
+void complete_assembler_table(machine_code assembler_table, table symbol_table) { /*TODO: <-- */
 
 }
 
-char* get_entry_symbol(char *line){ /*TODO: check function */
+char* get_entry_symbol(char *line) { /*TODO: check function */
 	char *word = (char*) malloc(sizeof(char));
 	word = strtok(line, " \t");
 	word = strtok(NULL, " \t");
@@ -85,7 +114,7 @@ char* get_entry_symbol(char *line){ /*TODO: check function */
 	return word;
 }
 
-int is_entey(FILE input_file) {
+int is_entey(FILE input_file) { /*check if there are entries in input file*/
 	char *word;
 	char line[MAX_LINE_LENGTH];
 
@@ -104,7 +133,7 @@ int is_entey(FILE input_file) {
 	return 0;
 }
 
-int is_external(FILE input_file) {
+int is_external(FILE input_file) { /*check if there are externals in input file*/
 	char *word;
 	char line[MAX_LINE_LENGTH];
 
@@ -123,21 +152,30 @@ int is_external(FILE input_file) {
 	return 0;
 }
 
-void fill_entries_output_file(symbol_table, FILE *entries_output_file) {/*TODO: <-- */
+char* cut_input_file_name_ending(char *input_file_name) { /*clear '.as' ending from input file name*/
+	char *input_name = (char*) malloc(sizeof(char));
+	strcpy(input_name, input_file_name);
+	strtok(input_name, ".as");
+	return input_name;
+}
+
+void fill_entries_output_file(table symbol_table, FILE *entries_output_file) {/*TODO: <-- */
+	table *current = symbol_table;
+	while (current != NULL && current->type == "entry") {/*TODO: check how it works exactly */
+		fprintf(entries_output_file, "%s,", current->key);
+		fprintf(entries_output_file, "%s,", current->baseadress);
+		fprintf(entries_output_file, "%s\n", current->offset);
+
+		current = current->next;
+	}
+}
+
+void fill_externals_output_file(machine_code assembler_table, FILE *externals_output_file) {/*TODO: <-- */
 
 }
 
-void fill_externals_output_file(assembler_table, FILE *externals_output_file) {/*TODO: <-- */
+void fill_object_output_file(machine_code assembler_table, FILE *object_output_file) {/*TODO: <-- */
 
 }
 
-void fill_object_output_file(assembler_table, FILE *object_output_file) {/*TODO: <-- */
-}
-
-
-/* TODO: delete
-int main(void) {
-	puts("!!!Hello World!!!");
-	return EXIT_SUCCESS;
-}*/
 
--- second_pass.h
+++ second_pass.h
@@ -7,18 +7,18 @@
 void second_pass_handle(FILE *input_file);
 
 /*adds the word entry in given symbol line of symbol table*/
-void add_entry_to_symbol_table(char *symbol);
+void add_entry_to_symbol_table(table symbol_table, char *symbol);
 
 /*fills in the empty assembler table line after the first pass*/
-void complete_assembler_table(assembler_table, symbol_table);
+void complete_assembler_table(machine_code assembler_table, table symbol_table);
 
 /*fills entry output file*/
-void fill_entries_output_file(symbol_table, FILE *entries_output_file);
+void fill_entries_output_file(table symbol_table, FILE *entries_output_file);
 
 /*fills externals output file*/
-void fill_externals_output_file(assembler_table, FILE *externals_output_file);
+void fill_externals_output_file(machine_code assembler_table, FILE *externals_output_file);
 
 /*fills object output file*/
-void fill_object_output_file(assembler_table, FILE *object_output_file);
+void fill_object_output_file(machine_code assembler_table, FILE *object_output_file);
 
 #endif
